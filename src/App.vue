e8f43f55a7f5693bfa0caeb5c8d9539e694ea1b1
Shantanu Dutta <shantanudutta1994@gmail.com>
2020-12-06 13:53:49 +0530

    1. Deleted template hello world component. 2. Added basic details for user. 3. Added ability to follow/ unfollow user.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }} <strong>Followers: </strong>
+    {{ followers }}
+    <button type="button" v-if="!following" @click="followUser">Follow</button>
+    <button type="button" v-if="following" @click="unFollowUser">
+      UnFollow
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      followers: 0,
+      following: false,
+      user: {
+        id: 1,
+        username: "iAmShaan",
+        firstName: "Shantanu",
+        lastName: "Dutta",
+        email: "shantanudutta1994@gmail.com",
+        isAdmin: true,
+      },
+    };
+  },
+  watch: {
+    followers(newFollowersCount, oldFollowersCount) {
+      const gainedOrLost =
+        oldFollowersCount < newFollowersCount ? "gained" : "lost";
+      console.info(`${this.user.username} has ${gainedOrLost} a follower!`);
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      if (this.following) {
+        return;
+      }
+      this.followers++;
+      this.following = true;
+    },
+    unFollowUser() {
+      if (!this.following) {
+        return;
+      }
+      this.followers--;
+      this.following = false;
+    },
+  },
+};
 </script>
 
 <style>
@@ -19,8 +62,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
